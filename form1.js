ce0b6a3dc98d8cea6fad4c6364a2ac2cc81da6b8
Ganesh <ganeshpai12kv@gmail.com>
2021-04-13 23:11:33 +0530

    Resolved errors

--- form1.js
+++ form1.js
@@ -16,27 +16,34 @@ let emailregex = /^([a-zA-Z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})$/;
 let pwdregex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
 let mobregex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/im;
 
-function validate() {
+function validatewithstrength() {
 
 	if (!emailregex.test(email.value)) {
 		emailerr.innerHTML = "Email format is Invalid.";
 		emailerr.style.color = "red";
 		document.getElementById("InputEmail").style.border = "solid 3px red";
 		return false;
-	} else if (!pwdregex.test(pwd.value)) {
-		pwderr.innerHTML = "password format is Invalid.";
-		pwderr.style.color = "red";
-		document.getElementById("InputPassword").style.border = "solid 3px red";
-		return false;
-
 	} else if (!mobregex.test(mobile.value)) {
 		moberr.innerHTML = "Mobile number format is Invalid.";
 		moberr.style.color = "red";
 		document.getElementById("InputMobile").style.border = "solid 3px red";
 		return false;
 
-	} else {
+	} else if (regExpWeak.test(pwd.value)) {
+		pwderr.innerHTML = "password  is weak";
+		pwderr.style.color = "red";
+		document.getElementById("InputPassword").style.border = "solid 3px red";
+		return false;
 
+	} else if (regExpMedium.test(pwd.value)) {
+		pwderr.innerHTML = "password  is not strong";
+		pwderr.style.color = "yellow";
+		document.getElementById("InputPassword").style.border = "solid 3px yellow";
+		return false;
+	} else if (regExpStrong.test(pwd.value)&&(pwd.value == cpwd.value)) {
+		pwderr.innerHTML = "password  is strong";
+		pwderr.style.color = "green";
+		document.getElementById("InputPassword").style.border = "solid 3px green";
 		return true;
-	}
+	} 
 }
