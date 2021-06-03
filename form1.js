6e44be3d8e4ad86015047214f051075a4fefc8d5
ganeshpaikv <66413465+ganeshpaikv@users.noreply.github.com>
2021-06-04 02:50:32 +0530

    Update form1.js

--- form1.js
+++ form1.js
@@ -5,6 +5,7 @@ let mobile = document.getElementById("InputMobile");
 
 let emailerr = document.getElementById("emailerror");
 let pwderr = document.getElementById("pwderror");
+let cpwderr = document.getElementById("cpwderror");
 let moberr = document.getElementById("moberror");
 
 let regExpWeak = /^[a-z]+$/;
@@ -23,27 +24,73 @@ function validatewithstrength() {
 		emailerr.style.color = "red";
 		document.getElementById("InputEmail").style.border = "solid 3px red";
 		return false;
-	} else if (!mobregex.test(mobile.value)) {
+	} if (!mobregex.test(mobile.value)) {
 		moberr.innerHTML = "Mobile number format is Invalid.";
 		moberr.style.color = "red";
 		document.getElementById("InputMobile").style.border = "solid 3px red";
 		return false;
 
-	} else if (regExpWeak.test(pwd.value)) {
+	// } else if  (String.IsNullOrEmpty(pwd.value)){
+	// 	pwderr.innerHTML = "password should not be empty";
+	// 	pwderr.style.color = "red";
+	// 	document.getElementById("InputPassword").style.border = "solid 3px red";
+	// 	return false;
+	// if (String.IsNullOrEmpty(textBox1.Text))
+	// {
+	// 	// Do something...
+	// }
+	
+
+// }  if (!pwdregex.test(pwd.value)&& (pwd.value!=cpwd.value)){
+// 	pwderr.innerHTML = "password format is Invalid.It should contain minimum 8 characters including Upper case,lower case, number and special characters";
+// 	pwderr.style.color = "red";
+// 	document.getElementById("InputPassword").style.border = "solid 3px red";
+// 	return false;
+
+
+
+ } if (regExpWeak.test(pwd.value)|| pwd.value=="") {
 		pwderr.innerHTML = "password  is weak";
 		pwderr.style.color = "red";
 		document.getElementById("InputPassword").style.border = "solid 3px red";
 		return false;
 
-	} else if (regExpMedium.test(pwd.value)) {
+	}  if (regExpMedium.test(pwd.value)) {
 		pwderr.innerHTML = "password  is not strong";
 		pwderr.style.color = "yellow";
 		document.getElementById("InputPassword").style.border = "solid 3px yellow";
 		return false;
-	} else if (regExpStrong.test(pwd.value)&&(pwd.value == cpwd.value)) {
+	//(regExpStrong.test(pwd.value)&&
+	}  if (regExpStrong.test(pwd.value)){
 		pwderr.innerHTML = "password  is strong";
 		pwderr.style.color = "green";
 		document.getElementById("InputPassword").style.border = "solid 3px green";
-		return true;
-	} 
+	}
+	if (cpwd.value==""){
+		pwderr.innerHTML = "password  is strong";
+		pwderr.style.color = "green";
+		document.getElementById("InputPassword").style.border = "solid 3px green";
+		cpwderr.innerHTML = "Confirm password required";
+		cpwderr.style.color = "red";
+		document.getElementById("cInputPassword").style.border = "solid 3px red";
+		return false;
+
+	}
+
+
+	 if (pwd.value!=cpwd.value){
+		pwderr.innerHTML = "password  is strong";
+		pwderr.style.color = "green";
+		document.getElementById("InputPassword").style.border = "solid 3px green";
+		cpwderr.innerHTML = "passwords do not match";
+		cpwderr.style.color = "red";
+		document.getElementById("cInputPassword").style.border = "solid 3px red";
+		return false;
+// }if (pwd.value==cpwd.value){
+// 	pwderr.innerHTML = "passwords are matching";
+// 	pwderr.style.color = "green";
+// 	document.getElementById("InputPassword").style.border = "solid 3px green";
+// 	return true;
+// }
 }
+	}
